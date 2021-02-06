725c9c82bdd4727c3df27d93b3e5030e30a12d1e
pujie guo <2515102G@student.gla.ac.uk>
2021-02-06 22:01:21 +0800

    first commit

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Rango says hey there partner!")
+   
+def about(reqoest):
+    return HttpResponse("Rango says here is the about page.")
